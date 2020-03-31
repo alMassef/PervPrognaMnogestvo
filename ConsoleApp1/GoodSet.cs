dde04f03a2c2a37433ef918837e55b766dfa25f8
AleksandrCF <aleksandrabz792@gmail.com>
2020-03-31 15:41:21 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/GoodSet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GoodSet<T> : IEnumerable
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public GoodSet() { }
+
+        public GoodSet(T item)
+        {
+            items.Add(item);
+        }
+
+        public GoodSet(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public void Add(T item)
+        {
+            if (items.Contains(item))
+            {
+                return;
+            }
+
+            items.Add(item);
+        }
+
+        public void Remowe(T item)
+        {
+            items.Remove(item);
+        }
+
+        public GoodSet<T> Union(GoodSet<T> set)
+        {
+            return new GoodSet<T>(items.Union(set.items));
+            //GoodSet<T> result = new GoodSet<T>();
+            //foreach (var item in items)
+            //{
+            //    result.Add(item);
+            //}
+
+            //foreach(var item in set.items)
+            //{
+            //    result.Add(item);
+            //}
+            //return result;
+        }
+
+        public GoodSet<T> Intersection(GoodSet<T> set)
+        {
+            return new GoodSet<T>(items.Intersect(set.items));
+        }
+
+        public GoodSet<T> Difference(GoodSet<T> set)
+        {
+            return new GoodSet<T>(items.Except(set.items));
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set1 = new GoodSet<int>(new int[] { 1, 2, 3, 4, 5 });
+            var set2 = new GoodSet<int>(new int[] { 4, 5, 6, 7, 8 });
+            var i = 9;
+
+            Console.Write("Union: ");
+            foreach (var item in set1.Union(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var item in set1.Intersection(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference A \\ B: ");
+            foreach (var item in set1.Difference(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference B \\ A: ");
+            foreach (var item in set2.Difference(set1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Add: ");
+            set1.Add(i);
+            Console.Write(set1);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
